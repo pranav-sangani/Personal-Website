f3853b1f054f7056ae4c29b46d79c37a969fadbb
Pranav Sangani <psangani@purdue.edu>
2025-02-03 18:38:04 -0500

    Update StartMessage.js

--- src/StartMessage.js
+++ src/StartMessage.js
@@ -6,10 +6,10 @@ function StartMessage() {
             <h2>Welcome To My Portfolio!</h2>
             <h3><i>Navigation through the website is done through the various icons to the left.<br/></i></h3>
             <h2><u>About Me</u></h2>
-            <h3>Hi, I’m Praneeth! I graduated with my BS in CS from Pitt in 2021. <br/> I am currently seeking new grad positions.</h3>
+            <h3>Hi, I’m Pranav! I'm currently a freshman pursuing my undergrad at Purdue University, where I will graduate with a Bachelor of Science degree in Computer Science. <br/> I am currently seeking new internship or job offers.</h3>
             <h2><u>About This Website</u></h2>
             <h3>Nope - you didn't teleport to the past! <br/>
-                This website is modeled after the first operating system that I ever used - Windows XP.<br/>
+                This website is modeled after the first operating system that I ever used (And, in my biased opinion, the best-looking operating system) - Windows XP! <br/>
                 Play some games, solve a puzzle, draw something and learn more about me!
             </h3>
         </div>
