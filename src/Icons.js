7f4c56082d46c4ec6a089f05f9eeb2561dd4cce0
Pranav Sangani <psangani@purdue.edu>
2025-02-20 20:23:14 -0500

    Update Icons.js

--- src/Icons.js
+++ src/Icons.js
@@ -29,7 +29,7 @@ function Icons({onClickPaint, onClickSolitaire, onClickMinesweeper, onClickPuzzl
                     <img src={ie_icon} alt="Linkedin Icon"/></a>
                 <p>Linkedin</p>
             </div>
-            <div className={'icon'}>
+            <div className={'icon'} style={{ marginLeft: '-5px' }}>
                 <a href={'https://pranav-sangani.github.io/SortingAlgorithmVisualizer/'}
                    target={'_blank'}
                    rel="noreferrer">
