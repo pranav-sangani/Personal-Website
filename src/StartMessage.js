8edf89beff90a9b10eced32c04ee4454eae456bf
Pranav Sangani <psangani@purdue.edu>
2025-09-30 19:44:52 -0400

    Update StartMessage.js

--- src/StartMessage.js
+++ src/StartMessage.js
@@ -9,7 +9,7 @@ function StartMessage() {
             <h3>Hi, I’m Pranav! I'm currently a sophomore pursuing my undergrad at Purdue University, where I will graduate with a Bachelor of Science degree in Computer Science. <br/> I am currently seeking new internships!</h3>
             <h2><u>About This Website</u></h2>
             <h3>Nope - you didn't teleport to the past! <br/>
-                This website is modeled after the first operating system that I ever used (And, in my biased opinion, the best-looking operating system) - Windows XP! <br/>
+                This website is modeled after the first operating system that I ever used (And, in my unbiased opinion, the best-looking operating system) - Windows XP! <br/>
                 Play some games, solve a puzzle, draw something and learn more about me!
             </h3>
         </div>
